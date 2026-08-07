eb1159cfa989c48d78e723c29cab0ae212c01515
Sparsh Agrawal <agrawalsparsh92@gmail.com>
2026-08-07 15:00:37 +0530

    Refactor StatCard components to use icons from lucide-react

--- frontend/src/pages/RegionalHead/RegionalDashboard.jsx
+++ frontend/src/pages/RegionalHead/RegionalDashboard.jsx
@@ -6,6 +6,12 @@ import DashboardHeader from "../../components/regionalHead/DashboardHeader";
 import StatCard from "../../components/regionalHead/StatCard";
 import RecentNotesheets from "../../components/regionalHead/RecentNotesheets";
 import DepartmentSummary from "../../components/regionalHead/DepartmentSummary";
+import {
+    FileText,
+    Clock3,
+    CircleCheckBig,
+    FilePlus2
+} from "lucide-react";
 
 const RegionalDashboard = () => {
   return (
@@ -19,33 +25,34 @@ const RegionalDashboard = () => {
 
         <DashboardHeader />
 
-        <div className="stats-grid">
+       <div className="stats-grid">
 
-          <StatCard
-            title="NOTESHEETS"
-            value="0"
-            icon="📄"
-          />
+    <StatCard
+        title="NOTESHEETS"
+        value={0}
+        icon={<FileText size={18} />}
+    />
 
-          <StatCard
-            title="PENDING"
-            value="0"
-            icon="🕒"
-          />
+    <StatCard
+        title="PENDING"
+        value={0}
+        icon={<Clock3 size={18} />}
+    />
 
-          <StatCard
-            title="APPROVED / COMPLETED"
-            value="0"
-            icon="✔"
-          />
+    <StatCard
+        title="APPROVED / COMPLETED"
+        value={0}
+        icon={<CircleCheckBig size={18} />}
+    />
 
-          <StatCard
-            title="MEMOS ISSUED"
-            value="2"
-            icon="➕"
-          />
+    <StatCard
+        title="MEMOS ISSUED"
+        value={2}
+        icon={<FilePlus2 size={18} />}
+    />
 
-        </div>
+</div>
+         
 
         <div className="dashboard-bottom">
 
