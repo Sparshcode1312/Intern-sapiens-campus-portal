a1dc92b5b35a789f07a899e3040a98243c3b1158
Sparsh Agrawal <agrawalsparsh92@gmail.com>
2026-08-08 13:03:02 +0530

    Add API utility with axios configuration

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const API_BASE_URL = "https://intern-sapiens-campus-portal.onrender.com";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
